0c7cc22b1c89669257dfdda36660d4526e6b8069
franmen <franmen05@gmail.com>
2026-03-31 08:39:46 -0400

    test: add liquidation approval config endpoint tests

---
+++ janus-backend/src/test/java/com/janus/payment/LiquidationApprovalConfigTest.java
@@ -0,0 +1,48 @@
+package com.janus.payment;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class LiquidationApprovalConfigTest {
+
+    @Test
+    @Order(1)
+    void testConfigEndpointReturnsApprovalRequired() {
+        // The seeded config has LIQUIDATION_APPROVAL_REQUIRED enabled
+        given()
+                .auth().basic("agent", "agent123")
+                .when().get("/api/operations/1/liquidation/config")
+                .then()
+                .statusCode(200)
+                .body("approvalRequired", is(true));
+    }
+
+    @Test
+    @Order(2)
+    void testConfigEndpointAccessibleByClient() {
+        given()
+                .auth().basic("client", "client123")
+                .when().get("/api/operations/1/liquidation/config")
+                .then()
+                .statusCode(200)
+                .body("approvalRequired", is(true));
+    }
+
+    @Test
+    @Order(3)
+    void testConfigEndpointRequiresAuth() {
+        given()
+                .when().get("/api/operations/1/liquidation/config")
+                .then()
+                .statusCode(401);
+    }
+}
