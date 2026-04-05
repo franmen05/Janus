f5ec13026ae5aad5eba9928da7c90047c48a88d8
franmen <franmen05@gmail.com>
2026-04-05 15:07:34 -0400

    fix: correct syntax for @RolesAllowed annotation in AuditResource

--- janus-backend/src/main/java/com/janus/audit/api/AuditResource.java
+++ janus-backend/src/main/java/com/janus/audit/api/AuditResource.java
@@ -32,7 +32,7 @@ public class AuditResource {
     OperationService operationService;
 
     @GET
-    @RolesAllowed("SUPERVISOR","ADMIN")
+    @RolesAllowed({"SUPERVISOR","ADMIN"})
     public List<AuditLogResponse> list(@QueryParam("username") String username,
                                        @QueryParam("from") String from,
                                        @QueryParam("to") String to) {
